37e2d356f3c624b850934d91c72cc7260808921e
Surv <83309545+SurvExE-Pc@users.noreply.github.com>
2023-04-06 16:13:17 -0600

    v2.0 Source code
    The source code for v2.0 of my version of tims adventures.

---
+++ headers.h
@@ -0,0 +1,16 @@
+#pragma once	
+
+#include <iostream>
+using namespace std;
+#include <direct.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <ShObjIdl.h>
+#include <ShObjIdl_core.h>
+#include <Lmcons.h>
+#include "shlobj.h"
+#include "stdafx.h"
